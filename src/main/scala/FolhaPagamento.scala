699750c6c24a56518d9220d355049d506583a80d
Kaio Cesar <tecnico.kaio@gmail.com>
2019-09-15 11:08:56 -0300

    sbt buld

--- src/main/scala/FolhaPagamento.scala
+++ src/main/scala/FolhaPagamento.scala
@@ -1,4 +1,4 @@
-object CubeCalculator extends App {
+object FolhaPagamento extends App {
   def cube(x: Int) = {
     x * x * x
   }
---
+++ src/test/scala/FolhaPagamentoTest.scala
@@ -0,0 +1,5 @@
+class FolhaPagamentoTest extends org.scalatest.FunSuite {
+  test("FolhaPagamento.cube") {
+    assert(FolhaPagamento.cube(2) === 8)
+  }
+}
