b48d02ffbe8d4d3bd44cb1678c007f3d1647fa9e
Kaio Cesar <tecnico.kaio@gmail.com>
2019-09-15 11:36:00 -0300

    TDD FirsT

--- src/main/scala/FolhaPagamento.scala
+++ src/main/scala/FolhaPagamento.scala
@@ -1,5 +1,72 @@
 object FolhaPagamento extends App {
-  def cube(x: Int) = {
-    x * x * x
+
+  // :salario_base 3000
+  //   :base_horas 220
+  //   :jornada {:inicio 8 :refeicao 60 :termino 17 }
+  //   :insalubridade "média"
+  //   :periculosidade 0.30
+  //   :dependentes 2
+  //   :horas_extras {
+  //       :domingos_feriados 1
+  //       :dias_uteis 2
+  //     }
+  //   }
+  //   }})
+
+  def calcular_inss(salario_bruto: Float) : Float = {
+    salario_bruto
   }
+
+  def calcular_fgts(salario_bruto: Float) : Float = {
+    salario_bruto
+  }
+
+  def calcular_dependentes(salario_bruto: Float) : Float = {
+    salario_bruto
+  }
+
+  def calcular_irrf(salario_bruto: Float) : Float = {
+    salario_bruto
+  }
+
+  def calcular_horas_extras(salario_bruto: Float) : Float = {
+    salario_bruto
+  }
+
+  def calcular_dsr(salario_bruto: Float) : Float = {
+    salario_bruto
+  }
+
+  def calcular_vale_transporte(salario_bruto: Float) : Float = {
+    salario_bruto
+  }
+
+  def calcular_vale_alimentacao(salario_bruto: Float) : Float = {
+    salario_bruto
+  }
+
+  def calcular_adicional_noturno(salario_bruto: Float) : Float = {
+    salario_bruto
+  }
+
+  def calcular_adicional_insalubridade(salario_bruto: Float) : Float = {
+    salario_bruto
+  }
+
+  def calcular_salario_liquido(salario_bruto: Float) : Float = {
+    salario_bruto
+  }
+    
+  // calcular inss
+  // calcular o fgts
+  // calcular dependentes
+  // calcular o IRPF
+  // calcular as horas extras
+  // calcular DSR (descanso semanal remunerado sobre horas extras)
+  // calcular vale transporte
+  // calcular o vale alimentação
+  // adicional noturno
+  // adicional de insalubridade
+  // calcular salario liquido
+
 }
--- src/test/scala/FolhaPagamentoTest.scala
+++ src/test/scala/FolhaPagamentoTest.scala
@@ -1,5 +1,47 @@
 class FolhaPagamentoTest extends org.scalatest.FunSuite {
-  test("FolhaPagamento.cube") {
-    assert(FolhaPagamento.cube(2) === 8)
+
+  test("calcular inss") {
+    assert(FolhaPagamento.calcular_inss(3000) === 3100)
   }
+
+  test("calcular o fgts") {
+    assert(FolhaPagamento.calcular_fgts(2) === 8)
+  }
+
+  test("calcular dependentes") {
+    assert(FolhaPagamento.calcular_dependentes(2) === 8)
+  }
+
+  test("calcular o IRRF") {
+    assert(FolhaPagamento.calcular_irrf(2) === 8)
+  }
+
+  test("calcular as horas extras") {
+    assert(FolhaPagamento.calcular_horas_extras(2) === 8)
+  }
+
+  test("calcular DSR (descanso semanal remunerado sobre horas extras)") {
+    assert(FolhaPagamento.calcular_dsr(2) === 8)
+  }
+
+  test("calcular vale transporte") {
+    assert(FolhaPagamento.calcular_vale_transporte(2) === 8)
+  }
+
+  test("calcular o vale alimentação") {
+    assert(FolhaPagamento.calcular_vale_alimentacao(2) === 8)
+  }
+
+  test("adicional noturno") {
+    assert(FolhaPagamento.calcular_adicional_noturno(2) === 8)
+  }
+
+  test("adicional de insalubridade") {
+    assert(FolhaPagamento.calcular_adicional_insalubridade(2) === 8)
+  }
+
+  test("calcular salario liquido") {
+    assert(FolhaPagamento.calcular_salario_liquido(2) === 8)
+  }
+
 }
